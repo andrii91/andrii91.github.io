d254118fb4e60ef86170e6c7a3b9507e53d1bfeb
Andrii Litvin <al@gogmat.com>
2017-01-27 18:00:19 +0200

    fix

---
+++ other/content-marketing/src/js/component.js
@@ -0,0 +1,339 @@
+
+;(function ($) {
+    'use strict';
+
+    function setUp($el, settings) {
+        var width = $el.data('width'),
+            height = $el.data('height'),
+            ratio = ($el.data('ratio')) ? $el.data('ratio') : settings.default_ratio,
+            id = $el.data('youtube-id'),
+            padding_bottom,
+            innerHtml = [],
+            $thumb,
+            thumb_img,
+            loading_text = $el.text() ? $el.text() : settings.loading_text,
+            youtube_parameters = $el.data('parameters') || '';
+        
+        ratio = ratio.split(":");
+        
+        // width and height might override default_ratio value
+        if (typeof width === 'number' && typeof height === 'number') {
+          $el.width(width);
+          padding_bottom = height + 'px';
+        } else if (typeof width === 'number') {
+          $el.width(width);
+          padding_bottom = (width * ratio[1] / ratio[0]) + 'px';
+        } else {
+          width = $el.width();
+		      
+          // no width means that container is fluid and will be the size of its parent
+          if (width == 0) {
+            width = $el.parent().width();
+          }
+		      
+          padding_bottom = (ratio[1] / ratio[0] * 100) + '%';
+        }
+        
+        //
+        // This HTML will be placed inside 'lazyYT' container
+        
+        innerHtml.push('<div class="ytp-thumbnail">');
+        
+          // Play button from YouTube (exactly as it is in YouTube)
+          innerHtml.push('<div class="ytp-large-play-button"');
+          if (width <= 640) innerHtml.push(' style="transform: scale(0.563888888888889);"');
+          innerHtml.push('>');
+          innerHtml.push('<svg>');
+            innerHtml.push('<path fill-rule="evenodd" clip-rule="evenodd" fill="#1F1F1F" class="ytp-large-play-button-svg" d="M84.15,26.4v6.35c0,2.833-0.15,5.967-0.45,9.4c-0.133,1.7-0.267,3.117-0.4,4.25l-0.15,0.95c-0.167,0.767-0.367,1.517-0.6,2.25c-0.667,2.367-1.533,4.083-2.6,5.15c-1.367,1.4-2.967,2.383-4.8,2.95c-0.633,0.2-1.316,0.333-2.05,0.4c-0.767,0.1-1.3,0.167-1.6,0.2c-4.9,0.367-11.283,0.617-19.15,0.75c-2.434,0.034-4.883,0.067-7.35,0.1h-2.95C38.417,59.117,34.5,59.067,30.3,59c-8.433-0.167-14.05-0.383-16.85-0.65c-0.067-0.033-0.667-0.117-1.8-0.25c-0.9-0.133-1.683-0.283-2.35-0.45c-2.066-0.533-3.783-1.5-5.15-2.9c-1.033-1.067-1.9-2.783-2.6-5.15C1.317,48.867,1.133,48.117,1,47.35L0.8,46.4c-0.133-1.133-0.267-2.55-0.4-4.25C0.133,38.717,0,35.583,0,32.75V26.4c0-2.833,0.133-5.95,0.4-9.35l0.4-4.25c0.167-0.966,0.417-2.05,0.75-3.25c0.7-2.333,1.567-4.033,2.6-5.1c1.367-1.434,2.967-2.434,4.8-3c0.633-0.167,1.333-0.3,2.1-0.4c0.4-0.066,0.917-0.133,1.55-0.2c4.9-0.333,11.283-0.567,19.15-0.7C35.65,0.05,39.083,0,42.05,0L45,0.05c2.467,0,4.933,0.034,7.4,0.1c7.833,0.133,14.2,0.367,19.1,0.7c0.3,0.033,0.833,0.1,1.6,0.2c0.733,0.1,1.417,0.233,2.05,0.4c1.833,0.566,3.434,1.566,4.8,3c1.066,1.066,1.933,2.767,2.6,5.1c0.367,1.2,0.617,2.284,0.75,3.25l0.4,4.25C84,20.45,84.15,23.567,84.15,26.4z M33.3,41.4L56,29.6L33.3,17.75V41.4z"></path>');
+            innerHtml.push('<polygon fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" points="33.3,41.4 33.3,17.75 56,29.6"></polygon>');
+          innerHtml.push('</svg>');
+          innerHtml.push('</div>'); // end of .ytp-large-play-button
+        
+        innerHtml.push('</div>'); // end of .ytp-thumbnail
+        
+        // Video title (info bar)
+        innerHtml.push('<div class="html5-info-bar">');
+        innerHtml.push('<div class="html5-title">');
+        innerHtml.push('<div class="html5-title-text-wrapper">');
+        innerHtml.push('<a id="lazyYT-title-', id, '" class="html5-title-text" target="_blank" tabindex="3100" href="//www.youtube.com/watch?v=', id, '">');
+        innerHtml.push(loading_text);
+        innerHtml.push('</a>');
+        innerHtml.push('</div>'); // .html5-title
+        innerHtml.push('</div>'); // .html5-title-text-wrapper
+        innerHtml.push('</div>'); // end of Video title .html5-info-bar
+
+        $el.css({
+            'padding-bottom': padding_bottom
+        })
+          .html(innerHtml.join(''));
+        
+        if (width > 640) {
+          thumb_img = 'maxresdefault.jpg';
+        } else if (width > 480) {
+          thumb_img = 'sddefault.jpg';
+        } else if (width > 320) {
+          thumb_img = 'hqdefault.jpg';
+        } else if (width > 120) {
+          thumb_img = 'mqdefault.jpg';
+        } else if (width == 0) { // sometimes it fails on fluid layout
+          thumb_img = 'hqdefault.jpg';
+        } else {
+          thumb_img = 'default.jpg';
+        }
+        
+        $thumb = $el.find('.ytp-thumbnail').css({
+            'background-image': ['url(//img.youtube.com/vi/', id, '/', thumb_img, ')'].join('')
+        })
+          .addClass('lazyYT-image-loaded')
+          .on('click', function (e) {
+            e.preventDefault();
+            if (!$el.hasClass('lazyYT-video-loaded') && $thumb.hasClass('lazyYT-image-loaded')) {
+              $el.html('<iframe src="//www.youtube.com/embed/' + id + '?autoplay=1&' + youtube_parameters + '" frameborder="0" allowfullscreen></iframe>')
+                .addClass('lazyYT-video-loaded');
+            }
+          });
+
+        /*$.getJSON('//gdata.youtube.com/feeds/api/videos/' + id + '?v=2&alt=json', function (data) {
+            $el.find('#lazyYT-title-' + id).text(data.entry.title.$t);
+        });*/
+
+    }
+
+    $.fn.lazyYT = function (newSettings) {
+      var defaultSettings = {
+        loading_text: 'Loading...',
+        default_ratio: '16:9',
+        callback: null, // ToDO execute callback if given
+        container_class: 'lazyYT-container'
+      };
+      var settings = $.extend(defaultSettings, newSettings);
+      
+      return this.each(function () {
+          var $el = $(this).addClass(settings.container_class);
+          setUp($el, settings);
+      });
+    };
+
+}(jQuery));
+
+(function($){
+    $.fn.viewportChecker = function(useroptions){
+        // Define options and extend with user
+        var options = {
+            classToAdd: 'visible',
+            classToRemove : 'invisible',
+            classToAddForFullView : 'full-visible',
+            removeClassAfterAnimation: false,
+            offset: 100,
+            repeat: false,
+            invertBottomOffset: true,
+            callbackFunction: function(elem, action){},
+            scrollHorizontal: false,
+            scrollBox: window
+        };
+        $.extend(options, useroptions);
+
+        // Cache the given element and height of the browser
+        var $elem = this,
+            boxSize = {height: $(options.scrollBox).height(), width: $(options.scrollBox).width()},
+            scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1 || navigator.userAgent.toLowerCase().indexOf('windows phone') != -1) ? 'body' : 'html');
+
+        /*
+         * Main method that checks the elements and adds or removes the class(es)
+         */
+        this.checkElements = function(){
+            var viewportStart, viewportEnd;
+
+            // Set some vars to check with
+            if (!options.scrollHorizontal){
+                viewportStart = $(scrollElem).scrollTop();
+                viewportEnd = (viewportStart + boxSize.height);
+            }
+            else{
+                viewportStart = $(scrollElem).scrollLeft();
+                viewportEnd = (viewportStart + boxSize.width);
+            }
+
+            // Loop through all given dom elements
+            $elem.each(function(){
+                var $obj = $(this),
+                    objOptions = {},
+                    attrOptions = {};
+
+                //  Get any individual attribution data
+                if ($obj.data('vp-add-class'))
+                    attrOptions.classToAdd = $obj.data('vp-add-class');
+                if ($obj.data('vp-remove-class'))
+                    attrOptions.classToRemove = $obj.data('vp-remove-class');
+                if ($obj.data('vp-add-class-full-view'))
+                    attrOptions.classToAddForFullView = $obj.data('vp-add-class-full-view');
+                if ($obj.data('vp-keep-add-class'))
+                    attrOptions.removeClassAfterAnimation = $obj.data('vp-remove-after-animation');
+                if ($obj.data('vp-offset'))
+                    attrOptions.offset = $obj.data('vp-offset');
+                if ($obj.data('vp-repeat'))
+                    attrOptions.repeat = $obj.data('vp-repeat');
+                if ($obj.data('vp-scrollHorizontal'))
+                    attrOptions.scrollHorizontal = $obj.data('vp-scrollHorizontal');
+                if ($obj.data('vp-invertBottomOffset'))
+                    attrOptions.scrollHorizontal = $obj.data('vp-invertBottomOffset');
+
+                // Extend objOptions with data attributes and default options
+                $.extend(objOptions, options);
+                $.extend(objOptions, attrOptions);
+
+                // If class already exists; quit
+                if ($obj.data('vp-animated') && !objOptions.repeat){
+                    return;
+                }
+
+                // Check if the offset is percentage based
+                if (String(objOptions.offset).indexOf("%") > 0)
+                    objOptions.offset = (parseInt(objOptions.offset) / 100) * boxSize.height;
+
+                // Get the raw start and end positions
+                var rawStart = (!objOptions.scrollHorizontal) ? $obj.offset().top : $obj.offset().left,
+                    rawEnd = (!objOptions.scrollHorizontal) ? rawStart + $obj.height() : rawStart + $obj.width();
+
+                // Add the defined offset
+                var elemStart = Math.round( rawStart ) + objOptions.offset,
+                    elemEnd = (!objOptions.scrollHorizontal) ? elemStart + $obj.height() : elemStart + $obj.width();
+
+                if (objOptions.invertBottomOffset)
+                    elemEnd -= (objOptions.offset * 2);
+
+                // Add class if in viewport
+                if ((elemStart < viewportEnd) && (elemEnd > viewportStart)){
+
+                    // Remove class
+                    $obj.removeClass(objOptions.classToRemove);
+                    $obj.addClass(objOptions.classToAdd);
+
+                    // Do the callback function. Callback wil send the jQuery object as parameter
+                    objOptions.callbackFunction($obj, "add");
+
+                    // Check if full element is in view
+                    if (rawEnd <= viewportEnd && rawStart >= viewportStart)
+                        $obj.addClass(objOptions.classToAddForFullView);
+                    else
+                        $obj.removeClass(objOptions.classToAddForFullView);
+
+                    // Set element as already animated
+                    $obj.data('vp-animated', true);
+
+                    if (objOptions.removeClassAfterAnimation) {
+                        $obj.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                            $obj.removeClass(objOptions.classToAdd);
+                        });
+                    }
+
+                // Remove class if not in viewport and repeat is true
+                } else if ($obj.hasClass(objOptions.classToAdd) && (objOptions.repeat)){
+                    $obj.removeClass(objOptions.classToAdd + " " + objOptions.classToAddForFullView);
+
+                    // Do the callback function.
+                    objOptions.callbackFunction($obj, "remove");
+
+                    // Remove already-animated-flag
+                    $obj.data('vp-animated', false);
+                }
+            });
+
+        };
+
+        /**
+         * Binding the correct event listener is still a tricky thing.
+         * People have expierenced sloppy scrolling when both scroll and touch
+         * events are added, but to make sure devices with both scroll and touch
+         * are handles too we always have to add the window.scroll event
+         *
+         * @see  https://github.com/dirkgroenen/jQuery-viewport-checker/issues/25
+         * @see  https://github.com/dirkgroenen/jQuery-viewport-checker/issues/27
+         */
+
+        // Select the correct events
+        if( 'ontouchstart' in window || 'onmsgesturechange' in window ){
+            // Device with touchscreen
+            $(document).bind("touchmove MSPointerMove pointermove", this.checkElements);
+        }
+
+        // Always load on window load
+        $(options.scrollBox).bind("load scroll", this.checkElements);
+
+        // On resize change the height var
+        $(window).resize(function(e){
+            boxSize = {height: $(options.scrollBox).height(), width: $(options.scrollBox).width()};
+            $elem.checkElements();
+        });
+
+        // trigger inital check if elements already visible
+        this.checkElements();
+
+        // Default jquery plugin behaviour
+        return this;
+    };
+  
+  
+})(jQuery);
+
+$(document).ready(function() { 
+  $('.wake-btn').click(function(){
+    $('.wake-none').show();
+    $('.overlay').show();
+  });
+  $('.overlay, .close-m').click(function() {
+    $('.wake-none').hide();
+    $('.overlay').hide();
+  });
+  $('.box-check').click(function() {
+    $(this).parent('.acordion-box').toggleClass('active');
+  });
+  $('.lock-click').click(function() {
+    $(this).parents('.lock-list li').toggleClass('active');
+  });
+  $('.js-lazyYT').click(function() {
+    $(this).addClass('active');
+  });
+  $('.price-check li').click(function() {
+    $(this).parents('.price-box').find('.price-check li').removeClass('active');
+    $(this).addClass('active');
+    $(this).parents('.price-box').find('.price-summ').removeClass('active');
+    $(this).parents('.price-box').find('#'+$(this).data('check')).addClass('active');
+  });
+  $('.js-lazyYT').lazyYT();
+  
+  $('.program-item').viewportChecker({
+      classToAdd: 'visible',
+      offset: "50%",
+      repeat: true
+  });
+  $('.modal-btn').click(function() {
+    $('#' + $(this).data('modal')).show();
+      $('#' + $(this).data('modal')).animate({
+          opacity: 1,  });
+    $('body').addClass('overl-h');
+    $('.overlay').show();
+  });
+  $('.overlay, .close-m').click(function() {
+    $('body').removeClass('overl-h');
+    $('.modal').hide();
+    $('.overlay').hide();
+    $('.modal').animate({
+          opacity: 0,  });
+  });
+});
+$(window).scroll(function() {
+    return $('.head').toggleClass("header_fixed", $(window).scrollTop() > 0);
+});
+
+(function() {
+  var $body = document.body
+  , $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+
+  if ( typeof $menu_trigger !== 'undefined' ) {
+    $menu_trigger.addEventListener('click', function() {
+      $body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+    });
+  }
+
+}).call(this);
